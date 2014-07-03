28208182d8e694b7678f25b9a88644b88850db72
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2014-07-03 19:04:28 +0200

    Move rendering code to its own namespace

--- src/random_graph_visualization/core.cljs
+++ src/random_graph_visualization/core.cljs
@@ -1,4 +1,7 @@
-(ns random-graph-visualization.core)
+(ns random-graph-visualization.core
+  (:require [random-graph-visualization.render :refer [render-graph
+                                                       create-force-layout
+                                                       create-svg]]))
 
 (enable-console-print!)
 
@@ -14,80 +17,6 @@
            {"source" 0 "target" 2}
            {"source" 0 "target" 3}
            {"source" 0 "target" 4}]})
-
-(defn create-force-layout
-  [width height]
-  (-> js/d3 .-layout
-      (.force)
-      (.charge -120)
-      (.linkDistance 30)
-      (.size (array width height))))
-
-(def margin {:top 50
-             :bottom 50
-             :left 50
-             :right 50})
-
-(defn create-svg
-  [width height]
-  (-> js/d3
-      (.select "body")
-      (.append "svg")
-      (.attr "width" (+ (:left margin) width (:right margin))) 
-      (.attr "height" (+ (:bottom margin) height (:top margin)))
-      (.append "g")
-      (.attr "transform"
-             (str "translate(" (:left margin) "," (:top margin) ")"))))
-
-(defn start-force-layout
-  [force graph]
-  (-> force
-      (.nodes (aget graph "nodes"))
-      (.links (aget graph "links"))
-      .start))
-
-(defn create-links
-  [svg graph]
-  (-> svg
-      (.selectAll ".link")
-      (.data (aget graph "links"))
-      (.enter)
-      (.append "line")
-      (.attr "class" "link")
-      (.style "stroke-width" 1)))
-
-(defn create-nodes
-  [svg force graph]
-  (-> svg
-      (.selectAll ".node")
-      (.data (aget graph "nodes"))
-      (.enter)
-      (.append "circle")
-      (.attr "class" "node")
-      (.attr "r" 5)
-      (.attr "data-n" #(:id %))
-      (.style "fill" "cyan")
-      (.call (aget force "drag"))))
-
-(defn on-tick-handler
-  [links nodes]
-  (fn []
-    (-> links
-        (.attr "x1" #(-> % .-source .-x))
-        (.attr "y1" #(-> % .-source .-y))
-        (.attr "x2" #(-> % .-target .-x))
-        (.attr "y2" #(-> % .-target .-y)))
-    (-> nodes
-        (.attr "cx" #(aget % "x"))
-        (.attr "cy" #(aget % "y")))))
-
-(defn render-graph
-  [force svg graph]
-  (let [json-graph (clj->js graph)]
-    (start-force-layout force json-graph)
-    (let [links (create-links svg json-graph)
-          nodes (create-nodes svg force json-graph)]
-      (.on force "tick" (on-tick-handler links nodes)))))
 
 (let [width 650
       height 500]

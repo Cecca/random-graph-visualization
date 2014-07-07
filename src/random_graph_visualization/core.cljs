18d95a0c61b5d6e6d24978e2d0ea8ef06433f135
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2014-07-07 13:15:45 +0200

    Add working example of slider

--- src/random_graph_visualization/core.cljs
+++ src/random_graph_visualization/core.cljs
@@ -2,7 +2,10 @@
   (:require [random-graph-visualization.graph :refer [graph-gnp]]
             [random-graph-visualization.render :refer [render-graph
                                                        create-force-layout
-                                                       create-svg]]))
+                                                       create-svg]]
+            [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [om.widget.slider :refer [slider]]))
 
 (enable-console-print!)
 
@@ -11,12 +14,26 @@
                       :avg-deg 0.0
                       :num-nodes 0}))
 
-(def rnd-graph
-  (graph-gnp 10 0.1))
+(comment
+  (def rnd-graph
+    (graph-gnp 10 0.1))
 
-(let [width 800
-      height 700]
-  (render-graph
-   (create-force-layout width height)
-   (create-svg width height)
-   rnd-graph))
+  (let [width 800
+        height 700]
+    (render-graph
+     (create-force-layout width height)
+     (create-svg width height)
+     rnd-graph)))
+
+(defn slider-widget
+  [data owner]
+  (reify
+    om/IRenderState
+    (render-state [this {:keys [val]}]
+      (dom/p nil
+             (str "The value is " val)
+             (slider :val owner)))))
+
+(om/root slider-widget app-state
+  {:target (. js/document (getElementById "slider"))})
+
